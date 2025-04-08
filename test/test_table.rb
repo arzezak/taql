0ea404d178a2a41205d1dd50e68fd04da19e2344
Ariel Rzezak <arzezak@gmail.com>
2025-04-08 18:52:45 -0300

    Refactor Table model

--- test/test_table.rb
+++ test/test_table.rb
@@ -9,6 +9,30 @@ class TestTable < Minitest::Test
     ])
   end
 
+  def test_that_it_returns_columns
+    columns = [
+      ["name", "Alice", "Bob", "Charlie"],
+      ["age", "30", "25", "35"]
+    ]
+
+    assert_equal columns, @table.columns
+  end
+
+  def test_that_it_returns_headers
+    assert_equal ["name", "age"], @table.headers
+  end
+
+  def test_that_it_returns_rows
+    rows = [
+      ["name", "age"],
+      ["Alice", "30"],
+      ["Bob", "25"],
+      ["Charlie", "35"]
+    ]
+
+    assert_equal rows, @table.rows
+  end
+
   def test_that_it_outputs_a_table
     output = <<~OUTPUT
       +---------+-----+
@@ -20,57 +44,14 @@ class TestTable < Minitest::Test
       +---------+-----+
     OUTPUT
 
+    @table.print
+
     assert_output(output) { puts @table.print }
   end
 
-  def test_that_it_returns_headers
-    headers = ["name", "age"]
+  def test_that_it_renders_nothing
+    empty_table = Taql::Table.new([])
 
-    assert_equal headers, @table.headers
-  end
-
-  def test_that_it_returns_rows
-    rows = {
-      "name" => ["Alice", "Bob", "Charlie"],
-      "age" => [30, 25, 35]
-    }
-
-    assert_equal rows, @table.rows
-  end
-
-  def test_that_it_returns_rows_with_headers
-    rows_with_headers = {
-      "name" => ["name", "Alice", "Bob", "Charlie"],
-      "age" => ["age", 30, 25, 35]
-    }
-
-    assert_equal rows_with_headers, @table.rows_with_headers
-  end
-
-  def test_that_it_outputs_separators
-    assert_equal "+---------+-----+", @table.separator
-  end
-
-  def test_rows_max_length
-    rows_max_length = {
-      "name" => 7,
-      "age" => 3
-    }
-
-    assert_equal rows_max_length, @table.rows_max_length
-  end
-
-  def test_that_it_outputs_formatted_headers
-    assert_equal "| NAME    | AGE |", @table.formatted_headers
-  end
-
-  def test_that_it_returns_formatted_entries
-    formatted_entries = [
-      "| Alice   | 30  |",
-      "| Bob     | 25  |",
-      "| Charlie | 35  |"
-    ]
-
-    assert_equal formatted_entries, @table.formatted_entries
+    assert_output("\n") { puts empty_table.print }
   end
 end
