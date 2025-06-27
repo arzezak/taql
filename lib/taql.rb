267ae9705ecfc8046e31316953eced2ad0ca9730
Ariel Rzezak <arzezak@gmail.com>
2025-06-27 09:42:30 -0300

    Provide default options

--- lib/taql.rb
+++ lib/taql.rb
@@ -5,7 +5,7 @@ require_relative "taql/version"
 module Taql
   class Error < StandardError; end
 
-  def self.execute(query, options, connection: ActiveRecord::Base.connection)
+  def self.execute(query, options = {}, connection: ActiveRecord::Base.connection)
     connection.execute(query).tap do |result|
       if (results = result.entries).any?
         $stdout.puts Table.new(results, markdown: options[:markdown])
