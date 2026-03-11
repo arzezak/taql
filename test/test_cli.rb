e4e8fe30a567305aec5d78ed5f7f5d235c9b73be
Ariel Rzezak <arzezak@gmail.com>
2026-03-11 09:07:27 -0300

    Fix CLI to use passed argv instead of global ARGV

--- test/test_cli.rb
+++ test/test_cli.rb
@@ -2,18 +2,14 @@ require "test_helper"
 
 class TestCli < Minitest::Test
   def test_that_it_parses_queries
-    ARGV.replace(["SELECT * FROM users"])
-
-    cli = Taql::Cli.new(ARGV)
+    cli = Taql::Cli.new(["SELECT * FROM users"])
 
     assert_equal "SELECT * FROM users", *cli.query
     assert_equal false, cli.options[:markdown]
   end
 
   def test_that_it_parses_options
-    ARGV.replace(["--markdown", "SELECT * FROM users"])
-
-    cli = Taql::Cli.new(ARGV)
+    cli = Taql::Cli.new(["--markdown", "SELECT * FROM users"])
 
     assert_equal "SELECT * FROM users", *cli.query
     assert_equal true, cli.options[:markdown]
