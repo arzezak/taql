7257020afe9da28525011871ce1c6e98c58b4c07
Ariel Rzezak <arzezak@gmail.com>
2026-03-11 09:12:40 -0300

    Use lease_connection when available for Rails 7.2+ compat

--- lib/taql/railtie.rb
+++ lib/taql/railtie.rb
@@ -6,8 +6,13 @@ module Taql
 
     initializer "taql.initialize" do
       ActiveSupport.on_load(:active_record) do
-        Taql.instance_variable_set(:@default_connection, -> { ActiveRecord::Base.connection_pool.connection })
+        Taql.instance_variable_set(:@default_connection, method(:connection))
       end
+    end
+
+    def self.connection
+      pool = ActiveRecord::Base.connection_pool
+      pool.respond_to?(:lease_connection) ? pool.lease_connection : pool.connection
     end
   end
 end
